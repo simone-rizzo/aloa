- Added in the nn bbox the normalization layer for any input. - Fixed issue with the adult_shadow_noise dataset where putting labels throught the bb in the case of nn there was no scaling.

bboxes/nnbb.py
@@ -26,28 +26,33 @@ def normalize(ds, scaler=None, dataframe=True):
 
 
 class NeuralNetworkBlackBox(SklearnClassifierWrapper):
-    def __init__(self, model=None):
+    def __init__(self, model=None, scaler=None):
         if model:
             self.model = model
+            self.scaler = scaler
         else:
-            self.model = keras.models.load_model("models/nn/nn_blackbox_regolarized.h5")
+            self.model = keras.models.load_model("../models/nn/nn_blackbox_07_10_2022.h5")
+            self.scaler = pickle.load(open("../models/nn/nn_scaler.sav", 'rb'))
 
     def model(self):
         return self.model()
 
     def predict(self, x):
-        out = self.model.predict(x, verbose=False)
-        out = np.argmax(out, axis=1)
+        x, _ = normalize(x, self.scaler, False) # scaling layer
+        out = self.model.predict(x, verbose=False) # feedforward
+        out = np.argmax(out, axis=1) # argmax layer
         return out
 
     def predict_proba(self, x):
+        x, _ = normalize(x, self.scaler, False)  # scaling layer
         return self.model.predict(x, verbose=False)
 
-    def train_model(self, tr, tr_l, epochs=400):
+    def train_model(self, tr, tr_l, epochs=100):
         """
         Given the train set and train label, we train a NN
         with the same behaviour as the blackbox.
         """
+        tr, _ = normalize(tr, self.scaler, False)  # scaling layer # first we scale the values.
         inputs = keras.Input(shape=(tr.shape[1],))
         x = layers.Dense(256, activation="tanh")(inputs)
         # x = layers.Dropout(0.1)(x)
@@ -57,29 +62,29 @@ class NeuralNetworkBlackBox(SklearnClassifierWrapper):
         opt = tf.optimizers.Adam(learning_rate=0.005)
         model = keras.Model(inputs=inputs, outputs=output, name="nn_bb_model")
         model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-        model.fit(tr, tr_l, epochs=epochs, batch_size=16)
-        return NeuralNetworkBlackBox(model)
+        model.fit(tr, tr_l, epochs=epochs, batch_size=32)
+        return NeuralNetworkBlackBox(model, self.scaler)
 
 
 if __name__ == "__main__":
     bb = NeuralNetworkBlackBox()
 
     import pandas as pd
-    train_set = pd.read_csv("data/adult_original_train_set.csv")
-    test_set = pd.read_csv("data/adult_original_test_set.csv")
-    train_label = pd.read_csv("data/adult_original_train_label.csv")
-    test_label = pd.read_csv("data/adult_original_test_label.csv")
+    train_set = pd.read_csv("../data/adult_original_train_set.csv")
+    test_set = pd.read_csv("../data/adult_original_test_set.csv")
+    train_label = pd.read_csv("../data/adult_original_train_label.csv")
+    test_label = pd.read_csv("../data/adult_original_test_label.csv")
 
     # Here we normalize the training set and the test set
-    train_set, scaler = normalize(train_set)
-    test_set, _ = normalize(test_set, scaler)
+    """train_set, scaler = normalize(train_set)
+    test_set, _ = normalize(test_set, scaler)"""
 
     # Performances on training set
-    train_prediction = bb.predict(train_set)
+    train_prediction = bb.predict(train_set.values)
     report = classification_report(train_label, train_prediction)
     print(report)
 
     # Performances on test set
-    test_prediction = bb.predict(test_set)
+    test_prediction = bb.predict(test_set.values)
     report = classification_report(test_label, test_prediction)
     print(report)

core/original_lblonly.py
@@ -8,17 +8,16 @@ from sklearn.metrics import classification_report, accuracy_score, roc_curve
 import numpy as np
 from tqdm import tqdm
 from bboxes.nnbb import NeuralNetworkBlackBox
-
 from bboxes.rfbb import RandomForestBlackBox
 from core.attack import Attack
 from sklearn.metrics import roc_curve, accuracy_score, precision_score, classification_report
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
+
 class Original_lblonly(Attack):
     def __init__(self, bb, NOISE_SAMPLES, is_nn=False):
-        super().__init__(bb)
+        super().__init__(bb, is_nn)
         self.NOISE_SAMPLES = NOISE_SAMPLES
-        self.is_nn = is_nn
         self.scaler = None
 
     def carlini_binary_rand_robust(self, model, ds, ds_label, p, noise_samples=100, stddev=0.025, scaler=None):
@@ -26,11 +25,7 @@ class Original_lblonly(Attack):
         scores = []
         for row in tqdm(ds):
             label = ds_label[index]
-            if self.is_nn and scaler:
-                input_scaled, _ = self.normalize(np.array([row]), scaler, False)
-                y = model.predict(input_scaled)
-            else:
-                y = model.predict([row])[0]
+            y = model.predict(np.array([row]))[0]
             if y == label:
                 noise = np.random.binomial(1, p, (noise_samples, len(row[6:])))
                 x_sampled = np.tile(np.copy(row), (noise_samples, 1))
@@ -38,11 +33,7 @@ class Original_lblonly(Attack):
                                     where=noise.astype(bool)).astype(np.int32)
                 noise = stddev * np.random.randn(noise_samples, row[:6].shape[-1])
                 x_noisy = np.concatenate([x_sampled[:, :6] + noise, x_noisy], axis=1)
-                if self.is_nn:
-                     x_noisy, _ = self.normalize(x_noisy, scaler, False)
-                     noise_values = model.predict(x_noisy)
-                else:
-                    noise_values = model.predict(x_noisy)
+                noise_values = model.predict(x_noisy)
                 score = np.mean(np.array(list(map(lambda x: 1 if x == label else 0, noise_values))))
                 scores.append(score)
             else:  # Miss classification
@@ -58,12 +49,8 @@ class Original_lblonly(Attack):
         to imitate the black-box model.
         :return:
         """
-        if self.is_nn:
-            # Here we normalize the training set and the test set
-            self.noise_train_set_scaled, self.scaler = self.normalize(self.noise_train_set, dataFrame=True)
-            self.noise_test_set_scaled, _ = self.normalize(self.noise_test_set, self.scaler, dataFrame=True)
-        source_model = self.bb.train_model(self.noise_train_set_scaled.values, self.noise_train_label.values)
-        pred_tr_labels = source_model.predict(self.noise_train_set_scaled.values)
+        source_model = self.bb.train_model(self.noise_train_set.values, self.noise_train_label.values)
+        pred_tr_labels = source_model.predict(self.noise_train_set.values)
         tr_report = classification_report(self.noise_train_label, pred_tr_labels)
         print("Train report")
         print(tr_report)

models/nn/nn_black_box_training.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
+import pickle
 
 def normalize(ds, scaler=None):
     """
@@ -45,38 +45,40 @@ def load_nn_bb(filepath):
     return keras.models.load_model(filepath)
 
 
-train_set = pd.read_csv("data/adult_original_train_set.csv")
-test_set = pd.read_csv("data/adult_original_test_set.csv")
-train_label = pd.read_csv("data/adult_original_train_label.csv")
-test_label = pd.read_csv("data/adult_original_test_label.csv")
+train_set = pd.read_csv("../../data/adult_original_train_set.csv")
+test_set = pd.read_csv("../../data/adult_original_test_set.csv")
+train_label = pd.read_csv("../../data/adult_original_train_label.csv")
+test_label = pd.read_csv("../../data/adult_original_test_label.csv")
 
 # Here we normalize the training set and the test set
 train_set, scaler = normalize(train_set)
 test_set, _ = normalize(test_set, scaler)
+pickle.dump(scaler, open("nn_scaler.sav", 'wb'))
+# scaler = pickle.load(open(scalerfile, 'rb'))
 
 # Creation of the model
 model = get_nn_model(train_set.shape[1])
 
 # Undersampling of the dataset
-# undersample = RandomUnderSampler(sampling_strategy="majority")
-# tr, tr_l = undersample.fit_resample(train_set, train_label.values)
+undersample = RandomUnderSampler(sampling_strategy="majority")
+tr, tr_l = undersample.fit_resample(train_set, train_label.values)
 
 # Compilation of the model and training.
 opt = tf.optimizers.Adam(learning_rate=0.005)
 model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-model.fit(train_set, train_label, epochs=1, batch_size=32)
+model.fit(train_set, train_label, epochs=100, batch_size=32)
 
 # Performances on training set
-train_prediction = model.predict(train_set)
+train_prediction = model.predict(train_set.values)
 train_prediction = np.argmax(train_prediction, axis=1)
 report = classification_report(train_label, train_prediction)
 print(report)
 
 # Performances on test set
-test_prediction = model.predict(test_set)
+test_prediction = model.predict(test_set.values)
 test_prediction = np.argmax(test_prediction, axis=1)
 report = classification_report(test_label, test_prediction)
 print(report)
 
 # Saving the model
-model.save('nn_blackbox_regolarized.h5')
+model.save('nn_blackbox_07_10_2022.h5')